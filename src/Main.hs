711f466f184e12d7deec27362ed842893dde32e9
Dmitry Suzdalev <dimsuz@gmail.com>
2019-03-17 22:25:46 +0200

    Implement package validation on kt files

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Turtle
+import Turtle hiding (fp)
 import Prelude hiding (FilePath)
 import qualified Control.Foldl as Fold
 import Data.Maybe (listToMaybe, isJust, fromJust)
-import Data.Either (lefts)
-import Debug.Trace
+import Data.Either (lefts, rights)
+import Data.Bifunctor
 import qualified Data.Text as T
 
 data Module = Module
@@ -23,13 +23,11 @@ extractModuleName manifest = T.pack $ encodeString $ dirname $ parent $ parent $
 extractRootPath :: FilePath -> FilePath
 extractRootPath manifest = directory manifest </> "kotlin"
 
-packagePattern :: Pattern Text
-packagePattern = has ("package=" *> char '"' *> chars1 <* char '"')
-
 extractPackage :: FilePath -> IO (Maybe Text)
 extractPackage manifest = do
   let matches = listToMaybe . match packagePattern . lineToText <$> input manifest
   join <$> fold matches (Fold.find isJust)
+  where packagePattern = has ("package=" *> char '"' *> chars1 <* char '"')
 
 buildModule :: FilePath -> IO Module
 buildModule mf = do
@@ -55,20 +53,46 @@ validateModule m = do
       boolToEither hasPackage ("Module:  " <> name <> ". Package not found") m
     )
 
+validateKotlinPackage :: Module -> FilePath -> IO (Either FilePath ())
+validateKotlinPackage m fp = do
+  let expectedPackage = derivePackageName m fp
+  filePackage <- extractPackageFromKt fp
+  let isExpected = filePackage == Just expectedPackage
+  return $ if isExpected then Right () else Left fp
+
+convertValidationResult :: Module -> Either FilePath () -> Either Text Module
+convertValidationResult m = bimap (\errfp -> "Module: " <> name <> "\n\tfile " <> fname errfp <> " has invalid package") (\_ -> m)
+  where name = moduleName m
+        fname = stripModulePrefix m
+
+validateKotlinPackages :: Module -> IO (Either Text ())
+validateKotlinPackages m = do
+  files <- fold (findKotlinFiles (moduleSrcMainPath m)) Fold.list
+  validated <- mapM (validateKotlinPackage m) files
+  return $ mapM_ (convertValidationResult m) validated
+
 findKotlinFiles :: FilePath -> Shell FilePath
 findKotlinFiles = find (ends ".kt")
 
-testM = Module {moduleManifest = "/home/dima/projects/tbi-android/lib-preferences/src/main/AndroidManifest.xml", moduleSrcMainPath = "/home/dima/projects/tbi-android/lib-preferences/src/main/kotlin", modulePackage = Just "ru.appkode.base.lib.preferences", moduleName = "lib-preferences"}
+extractPackageFromKt :: FilePath -> IO (Maybe Text)
+extractPackageFromKt file = do
+  let matches = listToMaybe . match packagePattern . lineToText <$> input file
+  join <$> fold matches (Fold.find isJust)
+  where packagePattern = has ("package " *> chars1)
 
 toPackageName :: Text -> Text
 toPackageName = T.map (\c -> if c == '/' then '.' else c)
 
-derivePackageName :: Module -> FilePath -> Text
-derivePackageName m fp = let
+stripModulePrefix :: Module -> FilePath -> Text
+stripModulePrefix m fp = let
   -- no easy way to append '/' in the end, have to do this dance...
   srcMainPath = decodeString (encodeString (moduleSrcMainPath m) <> "/")
   relativeDir = directory . fromJust $ stripPrefix srcMainPath fp
-  rdirname = T.dropWhileEnd (\c -> c == '/' || c == '.') $ T.pack $ encodeString relativeDir
+  in T.pack $ encodeString (relativeDir </> filename fp)
+
+derivePackageName :: Module -> FilePath -> Text
+derivePackageName m fp = let
+  rdirname = T.dropWhileEnd (\c -> c == '/' || c == '.') $ stripModulePrefix m (directory fp)
   relativePackageName = toPackageName rdirname
   basePackage = fromJust $ modulePackage m
   in if T.null relativePackageName then basePackage else basePackage <> "." <> relativePackageName
@@ -76,15 +100,19 @@ derivePackageName m fp = let
 boolToEither :: Bool -> a -> b -> Either a b
 boolToEither test left right = if test then Right right else Left left
 
-printErrorList :: [Text] -> IO ()
-printErrorList errs = do
-  putStrLn "Found errors: "
+printErrorList :: Text -> [Text] -> IO ()
+printErrorList title errs = do
+  putStrLn ""
+  putStrLn $ T.unpack title
   mapM_ (putStrLn . T.unpack . ("  • " <>)) errs
 
 main :: IO ()
 main = do
   workDir <- pwd
-  modules <- findModules $ collapse (workDir <> "../tbi-android")
-  print $ head modules
-  validationErrors <- lefts <$> traverse validateModule modules
-  unless (null validationErrors) (printErrorList validationErrors)
+  modules <- findModules $ collapse (workDir <> "../base-project")
+  validatedModules <- traverse validateModule modules
+  let validationErrors = lefts validatedModules
+  let validModules = rights validatedModules
+  packageValidationErrors <- lefts <$> traverse validateKotlinPackages validModules
+  unless (null validationErrors) (printErrorList "Found errors, some modules will be skipped: "validationErrors)
+  if (null packageValidationErrors) then putStrLn "\nPackage validation: OK" else (printErrorList "Package errors: " packageValidationErrors)
