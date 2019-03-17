f41270c61360641012aa47aa21b191b7d905793e
Dmitry Suzdalev <dimsuz@gmail.com>
2019-03-17 19:04:40 +0200

    Finish building modules implementation. Basic validation

--- src/Main.hs
+++ src/Main.hs
@@ -4,38 +4,66 @@ module Main where
 import Turtle
 import Prelude hiding (FilePath)
 import qualified Control.Foldl as Fold
-import Data.Maybe (listToMaybe)
+import Data.Maybe (listToMaybe, isJust)
+import Data.Either (lefts)
+import qualified Data.Text as T
 
 data Module = Module
   { moduleManifest :: FilePath
   , moduleRootPath :: FilePath
-  , modulePackage :: Text
+  , modulePackage :: Maybe Text
+  , moduleName :: Text
   }
   deriving Show
 
-extractRootPath :: FilePath -> FilePath
-extractRootPath manifest = manifest </> "src" </> "main"
+extractModuleName :: FilePath -> Text
+extractModuleName manifest = T.pack $ encodeString $ dirname $ parent $ parent $ directory manifest
 
-extractPackage :: FilePath -> Text
-extractPackage manifest = "com.example"
+extractRootPath :: FilePath -> FilePath
+extractRootPath manifest = directory manifest </> "kotlin"
 
 packagePattern :: Pattern Text
 packagePattern = has ("package=" *> char '"' *> chars1 <* char '"')
 
-extractPackage' :: FilePath -> IO (Maybe Text)
-extractPackage' manifest = do
+extractPackage :: FilePath -> IO (Maybe Text)
+extractPackage manifest = do
   let matches = listToMaybe . match packagePattern . lineToText <$> input manifest
-  join <$> fold matches Fold.head
+  join <$> fold matches (Fold.find isJust)
+
+buildModule :: FilePath -> IO Module
+buildModule mf = do
+  package <- extractPackage mf
+  return (Module mf (extractRootPath mf) package (extractModuleName mf))
 
 findModules :: FilePath -> IO [Module]
 findModules dir = do
   files <- fold manifests Fold.list
-  return $ map (\mf -> Module mf (extractRootPath mf) (extractPackage mf)) files
+  mapM buildModule files
   where
-    manifests = findtree (invert (has "build")) (find (ends "AndroidManifest.xml") dir)
+    manifests = findtree (invert (has "build")) (find (ends "src/main/AndroidManifest.xml") dir)
+
+validateModule :: Module -> IO (Either Text Module)
+validateModule m = do
+  manifestExists <- testfile (moduleManifest m)
+  rootPathExists <- testdir (moduleRootPath m)
+  let hasPackage = isJust (modulePackage m)
+  let name = moduleName m
+  return (
+    boolToEither manifestExists ("Module:  " <> name <> ". No manifest found") m >>
+      boolToEither rootPathExists ("Module:  " <> name <> ". root path does not exist, expected 'src/kotlin'") m >>
+      boolToEither hasPackage ("Module:  " <> name <> ". Package not found") m
+    )
+
+boolToEither :: Bool -> a -> b -> Either a b
+boolToEither test left right = if test then Right right else Left left
+
+printErrorList :: [Text] -> IO ()
+printErrorList errs = do
+  putStrLn "Found errors: "
+  mapM_ (putStrLn . T.unpack . ("  • " <>)) errs
 
 main :: IO ()
 main = do
   modules <- findModules "../tbi-android"
-  print modules
-  putStrLn "hello world"
+  validationErrors <- lefts <$> traverse validateModule modules
+  unless (null validationErrors) (printErrorList validationErrors)
